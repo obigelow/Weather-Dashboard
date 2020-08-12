ad546f943211f2ebe076e1902182065eb029e35b
Oliver Bigelow <oelucy@gmail.com>
2020-08-11 23:57:14 -0700

    made initial api calls and turned unix time into real workable dates

--- script.js
+++ script.js
@@ -14,16 +14,17 @@ $.ajax({
         method: "GET"
     }).then(function(weatherResponse){
         console.log(weatherResponse.daily[0].dt)
-        var unixTime = weatherResponse.daily[0].dt;
+        dateArray = [];
+        for (var i = 0; i < 5; i++){
+        var unixTime = weatherResponse.daily[i].dt;
         var timeInMilliSeconds = unixTime*1000;
         var date = new Date(timeInMilliSeconds);
         var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
         var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1); //adding leading 0 if month less than 10 for mm format. Used less than 9 because javascriptmonths are 0 based.
         var year = date.getFullYear(); //full year in yyyy format
-        console.log(date)
-        console.log(day)
-        console.log(month)
-        console.log(year)
+        dateArray.push(month + "/" + day + "/" + year)
+        console.log(dateArray[i])
+        }
     })
 
 })
